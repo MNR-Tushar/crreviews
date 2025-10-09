2ed78050b8d03d89fda6cd24aabaf60f9db30754
MNR-Tushar <rahman23105101275@diu.edu.bd>
2025-10-09 17:04:09 +0600

    cr model create university and department model

--- cr/models.py
+++ cr/models.py
@@ -1,3 +1,48 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
+
+class University(models.Model):
+
+    Public = "Public"
+    Private = "Private"
+
+    university_type = [
+        (Public,"Public"),
+        (Private,"Private"),
+    ]
+
+
+    title =models.CharField(max_length=100,unique=True,blank=False,null=False)
+    type =models.CharField(max_length=100,choices=university_type)
+    address=models.CharField(max_length=150, blank=True, null=True)
+    slug=models.SlugField(null=True,blank=True,unique=True)
+
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+    dalete_at=models.DateTimeField(auto_now=True,blank=True,null=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug=slugify(self.title)
+        super().save(*args, **kwargs)
+    
+class Department(models.Model):
+
+    university=models.ForeignKey(University, on_delete=models.CASCADE,related_name='departments')
+    title=models.CharField(max_length=50,unique=True,blank=False,null=False)
+    code=models.CharField(max_length=50,unique=True,blank=True,null=True)
+    slug=models.SlugField(null=True,blank=True,unique=True)
+
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+    dalete_at=models.DateTimeField(auto_now=True,blank=True,null=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug=slugify(self.title)
+        super().save(*args, **kwargs)
