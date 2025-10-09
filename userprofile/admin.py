7a7c8d97b9c0adabcc1351136adf0933b3515e9e
MNR-Tushar <rahman23105101275@diu.edu.bd>
2025-10-09 19:53:38 +0600

    customusermodel and user model create of userprofile app

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display=['email','first_name','last_name' ,'student_id']
+    search_fields=['email','first_name','last_name','student_id','batch','section']
+    list_filter=['email','first_name','last_name','student_id','batch','section']
+    ordering=['first_name','last_name','batch']
---
+++ userprofile/manager.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self,first_name,last_name,email,password=None, **extra_fields):
+        
+
+        if not email:
+            raise ValueError("The user must have a email")
+        
+
+        email=self.normalize_email(email)
+        user=self.model(first_name=first_name,last_name=last_name,email=email,**extra_fields)
+        user.set_password(password)
+        user.save()
+
+    def create_superuser(self,first_name,last_name,email,password=None,**extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault('is_active',True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+        
+        return self.create_user(first_name,last_name,email,password,**extra_fields)
