1e75286eae0daf9dc7a86d24f167fcacf23e4234
Md Naimur Rahman <rahman23105101275@diu.edu.bd>
2025-10-09 17:04:09 +0600

    cr model create university and department model

--- cr/admin.py
+++ cr/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(University)
+class UniversityAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'address')
+    search_fields = ('title', 'type', 'address')
+    list_filter = ('type',)
+    ordering = ('title',)
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('university', 'title', 'code',)
+    search_fields = ('university', 'title', 'code')
+    list_filter = ('university',)
+    ordering = ('university',)
