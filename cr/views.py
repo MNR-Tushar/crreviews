3e1fe2946caf039be0e486e5b4f0c57f1aab8233
Md Naimur Rahman <rahman23105101275@diu.edu.bd>
2025-10-11 17:19:56 +0600

    cr card dynamic and other update

--- cr/views.py
+++ cr/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    cr=CrProfile.objects.all().order_by('-created_at')
+    total_university=University.objects.count()
+    total_department=Department.objects.count()
+    total_review=Review.objects.count()
+   
+
+    context={
+        'cr':cr,
+        'total_university':total_university,
+        'total_department':total_department,
+        'total_review':total_review,
+    }
+    return render(request,'home.html',context)
 
 def all_cr(request):
-    return render(request,'all_cr.html')
+    cr=CrProfile.objects.all()
+
+    context={
+        'cr':cr
+    }
+    return render(request,'all_cr.html',context)
 
 def latest_reviews(request):
     return render(request,'latest_reviews.html')
