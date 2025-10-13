fec31ea9481f295b170beffdd168827b3aca9da8
MNR-Tushar <rahman23105101275@diu.edu.bd>
2025-10-13 19:56:16 +0600

    pagination add and view done

--- cr/models.py
+++ cr/models.py
@@ -75,7 +75,7 @@ class CrProfile(models.Model):
 
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
-    daleted_at=models.DateTimeField(auto_now=True,blank=True,null=True)
+    deleted_at=models.DateTimeField(auto_now=True,blank=True,null=True)
 
     def __str__(self):
         return f"{self.name} ({self.user.email})"
