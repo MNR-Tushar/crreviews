191f8408fb4680683d0227ecd6775d20013bbca2
MNR-Tushar <rahman23105101275@diu.edu.bd>
2025-10-16 02:31:36 +0600

    update

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('login/',login,name="login"),
     path('registration/',registration,name="registration"),
     path('logout/',logout,name="logout"),
-    path('user_dashboard/',user_dasboard,name="user_dashboard"),
-    path('view_profile/',view_profile,name="view_profile"),
+    path('user_dashboard/<str:slug>',user_dasboard,name="user_dashboard"),
+    path('view_profile/<str:slug>/',view_profile,name="view_profile"),
     path('admin_dashboard/',admin_dashboard,name="admin_dashboard"),
 ]
