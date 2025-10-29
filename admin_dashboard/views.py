427768c5623dfd19bd7dda0cf03ac95821f87112
MNR-Tushar <rahman23105101275@diu.edu.bd>
2025-10-29 18:02:49 +0600

    new create admin dashboard and setup

---
+++ admin_dashboard/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.admin.views.decorators import staff_member_required
+from cr.models import *
+from userprofile.models import *
+
+@staff_member_required
+def admin_dashboard(request):
+
+    if not request.user.is_staff:
+        messages.error(request, "You don't have permission to access this page.")
+        return redirect('home')
+    
+   
+    pending_count = Review.objects.filter(is_anonymous=True, is_approved=False).count()
+    
+   
+    total_users = User.objects.count()
+    total_crs = CrProfile.objects.count()
+    total_reviews = Review.objects.count()
+    total_universities = University.objects.count()
+    total_departments = Department.objects.count()
+    
+    context = {
+        'pending_count': pending_count,
+        'total_users': total_users,
+        'total_crs': total_crs,
+        'total_reviews': total_reviews,
+        'total_universities': total_universities,
+        'total_departments': total_departments,
+    }
+    
+    return render(request, 'user_profile/admin_dashboard.html', context)
--- userprofile/views.py
+++ userprofile/views.py
@@ -404,34 +404,6 @@ def user_view(request,slug):
 
 
 
-@staff_member_required
-def admin_dashboard(request):
-
-    if not request.user.is_staff:
-        messages.error(request, "You don't have permission to access this page.")
-        return redirect('home')
-    
-   
-    pending_count = Review.objects.filter(is_anonymous=True, is_approved=False).count()
-    
-   
-    total_users = User.objects.count()
-    total_crs = CrProfile.objects.count()
-    total_reviews = Review.objects.count()
-    total_universities = University.objects.count()
-    total_departments = Department.objects.count()
-    
-    context = {
-        'pending_count': pending_count,
-        'total_users': total_users,
-        'total_crs': total_crs,
-        'total_reviews': total_reviews,
-        'total_universities': total_universities,
-        'total_departments': total_departments,
-    }
-    
-    return render(request, 'user_profile/admin_dashboard.html', context)
-
 @login_required
 def edit_user(request,slug):
 
