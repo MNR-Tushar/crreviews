51beddc9bc56f2605cff60c9634de305b2f6e4fa
MNR-Tushar <rahman23105101275@diu.edu.bd>
2025-11-05 21:53:21 +0600

    change model location of footer

--- admin_dashboard/admin.py
+++ admin_dashboard/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+@admin.register(Notice)
+class NoticeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'notice_type', 'is_active', 'created_at']
+    list_filter = ['is_active', 'notice_type', 'created_at']
+    search_fields = ['title', 'message']
+    list_editable = ['is_active']
+    ordering = ['-created_at']
+    
+    fieldsets = (
+        ('Notice Information', {
+            'fields': ('title', 'message', 'notice_type')
+        }),
+        ('Settings', {
+            'fields': ('is_active', 'link')
+        }),
+    )
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+    search_fields = ('name', 'email')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+@admin.register(Teck_Stack)
+class Teck_StackAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    search_fields = ('title',)
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+
+@admin.register(Developer_Profile)
+class Developer_ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+    search_fields = ('name', 'email')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
