44c1cc16c7ccab203f07fd12badbaa8959767384
MNR-Tushar <rahman23105101275@diu.edu.bd>
2025-11-13 01:48:27 +0600

    smtp

--- crreviews/settings.py
+++ crreviews/settings.py
@@ -173,19 +173,19 @@ AUTHENTICATION_BACKENDS = [
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'CR Reviews <infocrreviews@gmail.com>')
 
 # Email Configuration
-if DEBUG:
-    # Development - Console backend
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-else:
+# if DEBUG:
+#     # Development - Console backend
+#     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# else:
     # Production - Gmail SMTP
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # Your Gmail
-    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # App Password
-    EMAIL_TIMEOUT = 10
-    SERVER_EMAIL = DEFAULT_FROM_EMAIL
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # Your Gmail
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # App Password
+EMAIL_TIMEOUT = 10
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
 PASSWORD_RESET_TIMEOUT = 3600
 
